ebb8579b7dddefaa54c16a63241b4993719992d6
f6r6cqs <f6r6cqs@DAR-MXL7211GPW.fdconosur.com.ar>
2019-10-28 16:22:16 -0300

    mas cambios 4

---
+++ src/main/java/com/ml/exception/GeometryCalculationException.java
@@ -0,0 +1,8 @@
+package com.ml.exception;
+
+public class GeometryCalculationException extends Exception {
+
+	public GeometryCalculationException(String msg){
+		super(msg);
+	}
+}
--- src/main/java/com/ml/utils/WeatherProcessor.java
+++ src/main/java/com/ml/utils/WeatherProcessor.java
@@ -1,12 +1,18 @@
 package com.ml.utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.LinearRing;
 import org.locationtech.jts.geom.Polygon;
 
+import com.kenai.jffi.Array;
+import com.ml.exception.GeometryCalculationException;
+import com.ml.model.Position;
 import com.ml.model.SolarSystem;
 /*Utilizo el framework JTS para topologia el cual  dara soporte para simplificar
  * algunos calculos sobre geometria. 
@@ -18,22 +24,30 @@ public class WeatherProcessor {
 		
 	}
 	
-	private boolean isPlannetsAligned(SolarSystem solarSystem) {
-		  GeometryFactory gf = new GeometryFactory();
-
-//	        // Combierto la posicion de cada planeta  
-//		  Coordinate[] coords = solarSystem.getPlanets().stream(lines.split("\\s+"))
-//				.map(String::toUpperCase)
-//				.toArray(String[]::new);
-		  Coordinate[] coords  =
-		     new Coordinate[] {new Coordinate(0, 1), new Coordinate(0, -2),
-		                       new Coordinate(0, 2),new Coordinate(0, 1)};
-
-		  
-		  LinearRing lRing = gf.createLinearRing( coords );
+	//crea la figura que forman los tres planetas uniendo sus posiciones (sin el sol) si el area es cero entonces estan alineados
+	private boolean isPlannetsAlignedWithoutSun(SolarSystem solarSystem) throws GeometryCalculationException {
+		  Polygon planetsPolygon = createPolygonBetweenPlanets(solarSystem);
+		  if(planetsPolygon==null) {
+			  throw new GeometryCalculationException("Error de trazado en planetas contiguos");
+		  }
+//		  double perimeter = planets.getLength();		  
+		 return  (planetsPolygon.getArea()==0)?true:false;
+	}
+	
+	
+	public Polygon createPolygonBetweenPlanets(SolarSystem solarSystem) {
+		GeometryFactory gf = new GeometryFactory();	  
+	      // Combierto la posicion de cada planeta  
+		  ArrayList<Coordinate> coordsList = (ArrayList<Coordinate>) solarSystem.getPlanets().stream().map(x->{return positionToCoordinate(x.getPosition());}).collect(Collectors.toList());
+		  //Agrego una cuarta posicion que se corresponde a la del primer planeta de la lista. Esto es requisito en JTS para cerrar el poligono.
+		  coordsList.add(positionToCoordinate( solarSystem.getPlanets().get(0).getPosition()));
+		  LinearRing lRing = gf.createLinearRing( (Coordinate[]) coordsList.toArray() );
 		  LinearRing holes[] = null; // no aplica para este caso
 		  Polygon planets = gf.createPolygon(lRing, holes );
-		  double perimeter = planets.getLength();
-		return false;
+		return planets;
+	}
+	
+	private Coordinate positionToCoordinate(Position pos) {
+		return new Coordinate(pos.getCoordinateX(), pos.getCoordinateY());
 	}
 }
