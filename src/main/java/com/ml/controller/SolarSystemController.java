632077eb354bcd0c9007b745b2bc31979dbb8426
f6r6cqs <f6r6cqs@DAR-MXL7211GPW.fdconosur.com.ar>
2019-11-04 17:36:14 -0300

    Cambios controller

--- src/main/java/com/ml/controller/SolarSystemController.java
+++ src/main/java/com/ml/controller/SolarSystemController.java
@@ -1,10 +1,10 @@
 package com.ml.controller;
 
+import org.locationtech.jts.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ml.exception.SolarSystemException;
@@ -16,17 +16,32 @@ public class SolarSystemController {
 
 	@Autowired
 	WeatherService weatherService;
-	
-	@GetMapping("/clima/{dia}")
-	ResponseEntity<WeatherResponseDTO> findWeatherByDay(@PathVariable int dia){
-//		
+
+	@GetMapping("/clima")
+	ResponseEntity findWeatherByDay(@RequestParam(value = "dia") String day) {
+		//
 		WeatherResponseDTO weatherDto;
 		try {
-			weatherDto = weatherService.findWetherByDay(dia);
-			return new ResponseEntity<WeatherResponseDTO>(weatherDto,HttpStatus.OK);
+			if (!validateDay(day)) {
+				return ResponseEntity.badRequest().body("El dia debe estar comprendido entre 1 y 3650");
+			}
+			weatherDto = weatherService.findWetherByDay(Integer.parseInt(day));
+			return ResponseEntity.ok().body(weatherDto);
+		} catch (NumberFormatException e) {
+			return ResponseEntity.badRequest().body("El dia solo puede ser un valor numerico");
 		} catch (SolarSystemException e) {
-			return null;
+			return ResponseEntity.badRequest().body(e.toString());
 		}
 	}
 
+	private boolean validateDay(String day) throws NumberFormatException {
+
+		int dayConverted = Integer.parseInt(day);
+		if (dayConverted >= 1 && dayConverted <= 3560) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
--- src/main/java/com/ml/serverImpl/WeatherServiceImpl.java
+++ src/main/java/com/ml/serverImpl/WeatherServiceImpl.java
@@ -12,18 +12,18 @@ import com.ml.service.WeatherService;
 import com.ml.utils.WeatherProcessor;
 
 @Component
-public class WeatherServiceImpl implements WeatherService{
+public class WeatherServiceImpl implements WeatherService {
 
 	@Autowired
 	SolarSystem solarSytem;
-	
+
 	@Autowired
 	private WeatherDAO weatherDao;
-	
+
 	@Autowired
 	private WeatherProcessor weatherProcessor;
-	
-	//Devuelve el clima para un dia dado (dia 1 a 3650)
+
+	// Devuelve el clima para un dia dado (dia 1 a 3650)
 	@Override
 	public WeatherResponseDTO findWetherByDay(int day) throws SolarSystemException {
 		Weather weather = weatherDao.findByDayNumber(day);
@@ -31,36 +31,35 @@ public class WeatherServiceImpl implements WeatherService{
 	}
 
 	@Override
-	public void  predictWeatherForTeenYears() throws SolarSystemException {
+	public void predictWeatherForTeenYears() throws SolarSystemException {
 		try {
 			weatherProcessor.processWetherForTeenYears(solarSytem);
 		} catch (SolarSystemException e) {
 			throw e;
 		}
-	}	
-	
+	}
+
 	private WeatherResponseDTO buildWeatherToDTO(Weather weather) {
 
 		WeatherResponseDTO responseDTO = new WeatherResponseDTO();
 		responseDTO.setDia(new Integer(weather.getDayNumber()).toString());
-		switch(weather.getDayWhetherType()) {
-		  case RAIN:
-		    responseDTO.setClima("Se espera un dia lluvia");
-		    break;
-		  case DROUGHT:
-		    responseDTO.setClima("Habra sequia para esta jornada");
-		    break;
-		  case OPTIMAL:
-		    responseDTO.setClima("Se espera un hermoso dia");
-			    break;
-		  case UNDEFINED:
-			    responseDTO.setClima("Sin pronostico planetas en posicion invalida");
-			    break;
-		  default:
-		    // code block
+		switch (weather.getDayWhetherType()) {
+		case RAIN:
+			responseDTO.setClima("Se espera un dia lluvia");
+			return responseDTO;
+
+		case DROUGHT:
+			responseDTO.setClima("Habra sequia para esta jornada");
+			return responseDTO;
+		case OPTIMAL:
+			responseDTO.setClima("Se espera un hermoso dia");
+			return responseDTO;
+		case UNDEFINED:
+			responseDTO.setClima("Sin pronostico planetas en posicion invalida");
+			return responseDTO;
+		default:
+			return null;
 		}
-		
-		return null;
 	}
 
 }
